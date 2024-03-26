eec7f421056df55382b4da706680d6181fd8cdb7
VanilkaWvv <158144794+VanilkaWvv@users.noreply.github.com>
2024-03-26 08:36:12 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Biblioteca biblioteca = new Biblioteca("Carte de","Sergiu",2020,"Hololive");
-        biblioteca.display();
-        biblioteca.display(1);
-        biblioteca.display("Cocs");
-        biblioteca.find("carte");
-    }
-}
